ab012870b7a9faf5eee4c7c63d8cb97f32810401
fritzmg <fmg@inspiredminds.at>
2024-04-09 18:26:08 +0100

    fix dependencies

--- src/DependencyInjection/ContaoIsotopeProductTypeMessages.php
+++ src/DependencyInjection/ContaoIsotopeProductTypeMessages.php
@@ -15,7 +15,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class ContaoEyepinGatewayExtension extends Extension
+class ContaoIsotopeProductTypeMessagesExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
